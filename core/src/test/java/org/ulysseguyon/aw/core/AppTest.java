86058ca54ed0a901b34e2a7be967f00f3bf2094a
Ulysse Guyon <ulysse.guyon@etu.univ-nantes.fr>
2020-08-13 22:16:02 +0200

    [ENV] setup maven

---
+++ core/src/test/java/org/ulysseguyon/aw/core/AppTest.java
@@ -0,0 +1,19 @@
+package org.ulysseguyon.aw.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertEquals(true, true);
+    }
+}
